90f43aaefd1f7756923f65394f3ab4f0dbbb1cf8
neo <nsbikep@126.com>
2022-12-08 00:32:18 +0800

    增加数据库使用的方法

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "ViewController.h"
-
+#import "DTDBManager.h"
+#import "DTEventDBQueue.h"
 @interface ViewController ()
+
+@property (nonatomic , strong) DTDBManager *manager;
+@property (nonatomic, strong)DTEventDBQueue *dbQueue;
 
 @end
 
@@ -15,7 +19,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSString *filepath = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"TDData-event.plist"];
+    NSLog(@"%@",filepath);
+
+    self.dbQueue = [[DTEventDBQueue alloc] initWithDBPath:filepath];
+    [self.dbQueue addEvent:@"zxc" eventSyn:@"asd" createdAt:[[NSDate date] timeIntervalSince1970] completion:^(BOOL success) {
+        NSLog(@"%d",success);
+    }];
+    
+    [self.dbQueue addEvent:@"123" eventSyn:@"456" createdAt:[[NSDate date] timeIntervalSince1970] completion:^(BOOL success) {
+        NSLog(@"%d",success);
+    }];
+    
+    [self.dbQueue deleteEventsBySyn:@"asd" completion:^(BOOL success) {
+        NSLog(@"%d",success);
+    }];
+    
+    [self.dbQueue queryEventsCount:100 completion:^(NSArray<DTDBEventModel *> * _Nonnull result) {
+        NSLog(@"%@",result);
+    }];
+    
+    [self.dbQueue queryEventCountWithCompletion:^(NSUInteger count) {
+        NSLog(@"%zd",count);
+    }];
 }
 
 
