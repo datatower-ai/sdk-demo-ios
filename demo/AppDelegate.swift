67145ce238dde7c81e1f358c731f81433c2a5f4b
neo <nsbikep@126.com>
2022-12-12 23:01:16 +0800

    demo

---
+++ demo/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  AnalyticsDemo
+//
+//  Created by Apple02 on 2021/2/25.
+//
+
+import UIKit
+import DTSDK
+//import ROIQueryCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let channel :DTChannel = DTChannelAppStore
+        let logDegree :DTLogDegree = DTLogDegreeVerbose
+        let pro : Dictionary = [String: String]()
+        DataTower.initSDK(withAppID: "dt_271ec4a6aba6f401", channel: channel, isDebug: true, dtLogDegree: logDegree, commonProperties:pro)
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ demo/UserSetPropertyiesViewControoler.swift
@@ -0,0 +1,88 @@
+//
+//  UserSetPropertyiesViewControoler.swift
+//  AnalyticsDemo
+//
+//  Created by Apple06 on 2022/7/12.
+//
+import UIKit
+import Foundation
+import StoreKit
+import DTSDK
+
+class UserSetPropertiesViewConTroller: UIViewController {
+    
+  
+    @IBOutlet weak var useHintShow: UITextView!
+    var changeAge = true
+    var userSetProperties:[String:Any] = [:]
+    
+    override func viewDidLoad() {
+        showProperties(properties: userSetProperties)
+        
+    }
+    
+    
+    
+    @IBAction func userSet(_ sender: Any) {
+//        changeAge = !changeAge
+//        userSetProperties = [:]
+//        if changeAge {
+//            userSetProperties ["user_age"] = 23
+//        }else{
+//            userSetProperties ["user_age"] = 26
+//        }
+//        let course = ["语文","math","biology"]
+//        userSetProperties ["user_password"] = "7897070"
+//        userSetProperties["total_login_times"] = 1
+//        userSetProperties["course"] = course
+//
+//        DTAnalytics.userSet(properties: userSetProperties)
+//
+//        showProperties(properties: userSetProperties)
+    }
+    
+    @IBAction func userSetOnce(_ sender: Any) {
+//        var userSetOnceProperties:[String:Any] = [:]
+//        userSetOnceProperties["user_first_login_time"] = "2022-10-28"
+//        DTAnalytics.userSetOnce(properties: userSetOnceProperties)
+//        showProperties(properties: userSetOnceProperties)
+    }
+    
+    var timeOfLogin = 1
+    @IBAction func userAdd(_ sender: Any) {
+//        var userAddProperties:[String:Any] = [:]
+//        timeOfLogin=timeOfLogin+1
+//        userAddProperties["total_login_times"] = timeOfLogin
+//        DTAnalytics.userAdd(properties: userAddProperties)
+//        showProperties(properties: userAddProperties)
+    }
+    
+    
+    @IBAction func userUnset(_ sender: Any) {
+//        DTAnalytics.userUnset(_properties: "user_password")
+//        showProperties(text: "user_password")
+    }
+    
+    
+    @IBAction func userDelete(_ sender: Any) {
+//        DTAnalytics.userDelete()
+//        showProperties(text: "---userDelete----")
+    }
+    
+    
+    @IBAction func userAppend(_ sender: Any) {
+//        let course = ["History"]
+//        let userAppendProperties = ["course":course]
+//        DTAnalytics.userAppend(properties: userAppendProperties)
+//        showProperties(properties: userAppendProperties)
+        
+    }
+    
+    
+    func showProperties(properties:[String:Any])  {
+        useHintShow.text = properties.description
+    }
+    func showProperties(text:String)  {
+        useHintShow.text = text
+    }
+}
