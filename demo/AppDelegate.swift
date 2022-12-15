a6142fd044bf983060533aac8aa03d80543b3554
shijunxing <shijunxing@nodetower.com>
2022-12-15 10:59:26 +0800

    feature：初始化传入serverURL

--- demo/AppDelegate.swift
+++ demo/AppDelegate.swift
@@ -15,10 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appid = "dt_461a208fdd075c27"
+        let serverUrl = "https://report-inner.roiquery.com"
+//        let serverUrl = "https://test.roiquery.com"
+//        let serverUrl = "https://report.roiquery.com"
         let channel :DTChannel = DTChannelAppStore
         let logDegree :DTLogDegree = DTLogDegreeVerbose
-        let pro : Dictionary = [String: String]()
-        DataTower.initSDK(withAppID: "dt_461a208fdd075c27", channel: channel, isDebug: true, dtLogDegree: logDegree, commonProperties:pro)
+        DataTower.initSDK(withAppID:appid ,serverUrl: serverUrl, channel: channel, isDebug: true, dtLogDegree: logDegree)
 
         return true
     }
